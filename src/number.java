4ac67c5eed005dfb15742c0dce35a0482f0ba677
Zehra <48364643+zehra11@users.noreply.github.com>
2020-04-01 16:50:46 +0300

    commit

---
+++ src/number.java
@@ -0,0 +1,25 @@
+public class number {
+
+    public static void main(String[] args) {
+        int Num1 = 10;
+        short Short1 = 20;
+        long Long1 = 30;
+
+        double Double1 = 10.9;
+        float Float1 = 678.1f;
+        char Char1 = 'Z';
+        boolean Boolean1 = false;
+        byte Byte1 = 127;
+
+
+        System.out.println("Num1: "+Num1);
+        System.out.println("Short1: "+Short1);
+        System.out.println("Long1: "+Long1);
+        System.out.println("Double1: "+Double1);
+        System.out.println("Float: "+Float1);
+        System.out.println("Char1: "+Char1);
+        System.out.println("Boolean1: "+Boolean1);
+        System.out.println("Byte1: "+Byte1);
+    }
+
+}
